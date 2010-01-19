9fa3aaefe3eb94dc0a83ce41f744a8c7547491a8
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-01-19 14:53:11 +0000

    PositionStorage, Position, GroupCollection, Group have been added. Compilable but not executable yet.

---
+++ go-engine/Data/Position.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace go_engine.Data
+{
+    class Position
+    {
+        private MokuField _field;
+        private GroupCollection _groups = new GroupCollection();
+
+        private Position(int size)
+        {
+            _field = new MokuField(size);
+        }
+
+        public int Size { get { return _field.Size; } }
+
+        static Position CreateInitial(int size)
+        {
+            Position position = new Position(size);
+            return position;
+        }
+    }
+}
---
+++ go-engine/Data/PositionStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace go_engine.Data
+{
+    class PositionStorage
+    {
+        Position Move(Position originPosition, Point point, MokuState player)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Position Edit(Position originPosition, Point point, MokuState mokuState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
