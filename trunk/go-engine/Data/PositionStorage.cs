f51987289a11e4a7cd125e13390a20df16915eb6
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-01-20 05:25:39 +0000

    PositionStorage.Move interface has been slightly modified (number of eated stones has been added to return value)

--- trunk/go-engine/Data/PositionStorage.cs
+++ trunk/go-engine/Data/PositionStorage.cs
@@ -6,9 +6,20 @@ using Microsoft.Xna.Framework;
 
 namespace go_engine.Data
 {
+    struct Pair<T1, T2>
+    {
+    }
+
     class PositionStorage
     {
-        Position Move(Position originPosition, Point point, MokuState player)
+        /// <summary>
+        /// Произвести ход по правилам Го
+        /// </summary>
+        /// <param name="originPosition">Исходная позиция</param>
+        /// <param name="point">точка, в которую произведен ход</param>
+        /// <param name="player">игрок, который сделал ход</param>
+        /// <returns>новая позиция</returns>
+        Pair<Position, int> Move(Position originPosition, Point point, MokuState player)
         {
             throw new NotImplementedException();
         }
