fffd73e0467d0533d96a21a6d9262314c44847d6
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-01-24 21:33:31 +0000

    1. FindOppositeGroup has been added to Position 2. TestRunner project has been added for debug purpose

---
+++ TestRunner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using go_engine.Test;
+
+namespace TestRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PositionTest t = new PositionTest();
+            t.TestFirstMove();
+        }
+    }
+}
