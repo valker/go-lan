ed794b4820c45da5051546ae8ddf34a172842749
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-01-29 11:12:36 +0000

    Some comments added New PositionStorage constructor added Several tests stub added

--- go-engine/Data/ExceptionReason.cs
+++ go-engine/Data/ExceptionReason.cs
@@ -1,9 +1,21 @@
 namespace go_engine.Data
 {
+    /// <summary>
+    /// Задаёт причины возникновения исключения.
+    /// </summary>
     public enum ExceptionReason
     {
+        /// <summary>
+        /// Позиция занята камнем
+        /// </summary>
         Occupped,
+        /// <summary>
+        /// Нельзя оставлять свою группу мёртвой
+        /// </summary>
         CannotPlaceDead,
+        /// <summary>
+        /// Ход запрещён по правилу КО
+        /// </summary>
         Ko
     }
 }
--- go-engine/Data/GroupField.cs
+++ go-engine/Data/GroupField.cs
@@ -2,6 +2,9 @@ using Microsoft.Xna.Framework;
 
 namespace go_engine.Data
 {
+    /// <summary>
+    /// Задаёт матрицу ссылок на группы
+    /// </summary>
     internal class GroupField
     {
         private Group[,] _field;
@@ -10,6 +13,10 @@ namespace go_engine.Data
             _field = new Group[size,size];
         }
 
+        /// <summary>
+        /// Конструктор копирования
+        /// </summary>
+        /// <param name="parent">родительский объект</param>
         public GroupField(GroupField parent)
         {
             _field = (Group[,]) parent._field.Clone();
