70c58d8db469f502ca982ccd9b9e78412dacd428
valker.rus@gmail.com <valker.rus@gmail.com@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-11-26 14:46:19 +0000

    TextBased and WinForms drivers have been added

---
+++ trunk/GoLanClient/WinForms/Driver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GoLanClient.Engine;
+using GoLanClient.UI;
+
+namespace GoLanClient.WinForms
+{
+    class Driver
+    {
+        private IEngine _engine;
+
+        public Driver(IEngine engine)
+        {
+            _engine = engine;
+        }
+
+        public void Run()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new MainForm(_engine));
+        }
+    }
+}
