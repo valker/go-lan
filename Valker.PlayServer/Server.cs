9e206cd737e1990b5148b148a0763a9bc953b8e7
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-12-13 14:53:05 +0000

    playserver has been added

---
+++ Valker.PlayServer/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Valker.PlayServer
+{
+
+
+
+    public class Server
+    {
+
+        TcpListener listener = new TcpListener();
+        public ServerConfig Config { get; set; }
+        public Server(ServerConfig config) { Config = config; }
+        public void Start() 
+        {
+            listener.
+        }
+
+
+    }
+}
