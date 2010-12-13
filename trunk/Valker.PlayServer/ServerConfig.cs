e359e8b7b3cfbe1b22f6a10cf04bf84ac98e801e
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-12-13 14:53:05 +0000

    playserver has been added

---
+++ trunk/Valker.PlayServer/ServerConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Valker.PlayServer
+{
+    class ServerConfig
+    {
+        int Port { get; set; }
+    }
+}
---
+++ trunk/Valker.PlayServerStarter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Valker.PlayServerStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
