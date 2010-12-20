7324e9740465f818eb4220a142868f238f12936e
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-12-20 14:32:50 +0000

    transport interface to support different servers

---
+++ trunk/Valker.Api/ITransport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Valker.Api
+{
+    /// <summary>
+    /// Define an interface to abstract different message servers
+    /// </summary>
+    public interface ITransport
+    {
+    }
+}
