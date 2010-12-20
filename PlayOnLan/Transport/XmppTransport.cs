f85fb26983ba3ed745c05535c281ddbd9de4af0b
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2010-12-20 14:31:31 +0000

    xmpp transport initial

---
+++ PlayOnLan/Transport/XmppTransport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Valker.Api;
+
+namespace Valker.PlayOnLan.Transport
+{
+    class XmppTransport : ITransports 
+    {
+    }
+}
