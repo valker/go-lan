58b892aedbbce60e03472d544c3796b18fcf7818
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2011-02-17 14:55:06 +0000

    new message types added

---
+++ trunk/Valker.PlayOnLan.Server/Messages/Client/AcknowledgeRegistrationMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Valker.PlayOnLan.Api.Communication;
+
+namespace Valker.PlayOnLan.Server.Messages.Client
+{
+    class AcknowledgeRegistrationMessage : ClientMessage
+    {
+        #region Overrides of ClientMessage
+
+        public override void Execute(IClientMessageExecuter client, object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/Valker.PlayOnLan.Server/Messages/Server/DropNewPartyMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Valker.PlayOnLan.Api.Communication;
+
+namespace Valker.PlayOnLan.Server.Messages.Server
+{
+    class DropNewPartyMessage : ServerMessage
+    {
+        #region Overrides of ServerMessage
+
+        public override void Execute(IServerMessageExecuter server)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
