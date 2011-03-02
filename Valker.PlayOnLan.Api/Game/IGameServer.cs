de546bb01312db5e589abf3e6fe21e8e1d946307
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2011-03-02 08:08:33 +0000

    added missed files

---
+++ Valker.PlayOnLan.Api/Game/IGameServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Valker.PlayOnLan.Api.Game
+{
+    public interface IGameServer
+    {
+        /// <summary>
+        /// Register new party
+        /// </summary>
+        /// <remarks>TODO: parameters should be defined</remarks>
+        void RegisterNewParty(string playerName, IGameParameters parameters);
+        string Name { get; }
+        string ID { get; }
+    }
+}
