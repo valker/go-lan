3c059d2c8e594e3ab959cb48cf11112498b0c3ac
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2011-03-02 08:06:40 +0000

    added missed file

---
+++ trunk/Valker.TicTacToePlugin/TicTacToeServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Valker.PlayOnLan.Api.Game;
+
+namespace Valker.TicTacToePlugin
+{
+    class TicTacToeServer : IGameServer
+    {
+        private IGameType _gameType;
+
+        public TicTacToeServer(TicTacToeGame ticTacToeGame)
+        {
+            // TODO: Complete member initialization
+            this._gameType = ticTacToeGame;
+        }
+        public void RegisterNewParty(string playerName, IGameParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public string Name
+        {
+            get { return _gameType.Name; }
+        }
+
+        public string ID
+        {
+            get { return _gameType.ID; }
+        }
+    }
+}
