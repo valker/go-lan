395b1a4f2c6b08bf82b58ff370c44c1f4cb44c0e
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2011-03-14 20:16:13 +0000

    Copyrights added

--- Valker.PlayOnLan.Goban/Properties/AssemblyInfo.cs
+++ Valker.PlayOnLan.Goban/Properties/AssemblyInfo.cs
@@ -1,28 +1,26 @@
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Valker.PlayOnLan.Client")]
+[assembly: AssemblyTitle("Valker.PlayOnLan.Goban")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Valker.PlayOnLan.Client")]
-[assembly: AssemblyCopyright("Copyright � Valentin Pimenov 2011")]
+[assembly: AssemblyProduct("Valker.PlayOnLan.Goban.Properties")]
+[assembly: AssemblyCopyright("Copyright © Valentin Pimenov 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("d1b12042-5dca-4e51-8e10-f773730738a0")]
+[assembly: Guid("829b29e4-6525-4033-8028-aa947df3e376")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,9 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Valker.TicTacToePlugin/Properties/AssemblyInfo.cs
+++ Valker.TicTacToePlugin/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Valker.TicTacToePlugin")]
-[assembly: AssemblyCopyright("Copyright 2011")]
+[assembly: AssemblyCopyright("Copyright Valentin Pimenov 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
