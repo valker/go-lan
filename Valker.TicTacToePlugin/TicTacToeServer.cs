7b67436fd3eb2065a508a8510355384a684ec85f
valker.rus@gmail.com <valker.rus@gmail.com@02eeef48-011d-11df-adb7-1be7f7e3f636>
2011-03-16 13:42:47 +0000

    First implementation of tic-tac-toe allows to play on single computer. No judge at the moment.

--- Valker.TicTacToePlugin/TicTacToeServer.cs
+++ Valker.TicTacToePlugin/TicTacToeServer.cs
@@ -29,10 +29,10 @@ namespace Valker.TicTacToePlugin
 
         public void ProcessMessage(IPlayer sender, string message)
         {
-            switch (ExtractCommand(message))
+            switch (MessageUtils.ExtractCommand(message))
             {
                 case "M":
-                    var param = ExtractParams(message).Select(s => Convert.ToInt32(s)).ToArray();
+                    var param = MessageUtils.ExtractParams(message).Select(s => Convert.ToInt32(s)).ToArray();
                     foreach (var i in param)
                     {
                         if (i >= Parameters.Width || i < 0)
@@ -43,7 +43,7 @@ namespace Valker.TicTacToePlugin
 
                     _field[param[0], param[1]] = _stones[_currentPlayer];
 
-                    var msg = _createGameMessage(string.Format("FC<{0},{1},{2}>", param[0], param[1], _stones[_currentPlayer])).ToString();
+                    var msg = string.Format("FC<{0},{1},{2}>", param[0], param[1], _stones[_currentPlayer]);
                     SendMessageToPlayer(0, msg);
                     SendMessageToPlayer(1, msg);
 
@@ -52,20 +52,6 @@ namespace Valker.TicTacToePlugin
                     break;
 
             }
-        }
-
-        private static string[] ExtractParams(string message)
-        {
-            int left = message.IndexOf('<');
-            int right = message.IndexOf('>');
-            var part = message.Substring(left + 1, right - (left + 1));
-            return part.Split(',');
-        }
-
-        private static string ExtractCommand(string message)
-        {
-            var indexOf = message.IndexOf('<');
-            return indexOf == -1 ? message : message.Substring(0, indexOf);
         }
 
         public event EventHandler<OnMessageEventArgs> OnMessage = delegate { };
