e308d6d6b9cdeb8b935959f92252b8643c8b32fc
valker.rus@gmail.com <valker.rus@gmail.com@02eeef48-011d-11df-adb7-1be7f7e3f636>
2011-04-01 13:10:04 +0000

    Added ability to test xmpp or local configuration up to command line parameters Synchronization of party beginning Code style little improvement

---
+++ trunk/Valker.PlayOnLan.Client/NewAgentCreatingEventArgs.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Valker.PlayOnLan.Client
+{
+    public class NewAgentCreatingEventArgs : EventArgs
+    {
+        public string Name { get; set; }
+    }
+}
--- trunk/Valker.PlayOnLan.Client/ServerForm.cs
+++ trunk/Valker.PlayOnLan.Client/ServerForm.cs
@@ -1,47 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
-using Valker.PlayOnLan.Client.Communication;
-using Valker.PlayOnLan.Server;
 
 namespace Valker.PlayOnLan.Client
 {
-    public partial class ServerForm : Form
+    public sealed partial class ServerForm : Form
     {
-        private ServerImpl _server;
+        public event EventHandler<NewAgentCreatingEventArgs> NewAgentCreating = delegate { };
 
-        public ServerForm(ServerImpl server)
+        public ServerForm()
         {
-            _server = server;
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var transport = new LocalTransport();
-            _server.AddConnector(transport.ServerConnector);
-            var client = new ClientImpl(textBox1.Text, this,
-                                        new[]
-                                            {
-                                                new AgentInfo
-                                                    {ClientConnector = transport.ClientConnector, ClientIdentifier = ""}
-                                            });
-            client.AcceptedPlayer += AcceptedPlayer;
-            client.RegisterNewPlayer();
-        }
-
-        void AcceptedPlayer(object sender, AcceptedPlayerEventArgs e)
-        {
-            var client = (ClientImpl)sender;
-
-            if (e.Status)
-            {
-                var form = new MainForm(client);
-                form.Show(this);
-            }
-            else
-            {
-                client.Dispose();
-            }
+            NewAgentCreating(this, new NewAgentCreatingEventArgs() {Name = textBox1.Text});
         }
     }
 }
