806ca0535b58248441cf26e54570086372d04427
valker.rus@gmail.com <valker.rus@gmail.com@02eeef48-011d-11df-adb7-1be7f7e3f636>
2011-04-30 14:53:16 +0000

    GtkClient has been added

---
+++ GtkClient/ServerForm.cs
@@ -0,0 +1,12 @@
+using System;
+namespace GtkClient
+{
+	public partial class ServerForm : Gtk.Window
+	{
+		public ServerForm () : base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
