60a9a7047f5bb059f1afc48c3e356212232e677b
valker.rus <valker.rus@02eeef48-011d-11df-adb7-1be7f7e3f636>
2011-07-04 06:36:20 +0000

    added missed files

---
+++ trunk/Valker.PlayOnLan.Client/AuthentificationParams.cs
@@ -0,0 +1,10 @@
+namespace Valker.PlayOnLan.Client2008
+{
+    public class AuthentificationParams
+    {
+        public string Name { get; set; }
+        string Password { get; set; }
+
+        public string ServerName { get; set; }
+    }
+}
